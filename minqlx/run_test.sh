7c4061fc3916619cb456b51e396907ea03fd8ba7
Edgard Lindner <elindner@gmail.com>
2018-07-10 14:46:40 -0400

    delete spurious test runner

--- minqlx/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for test_file in $(ls *_test.py)
-do
-  echo
-  # echo -e "\x1B[92m>> ${test_file} <<\x1B[0m"
-  echo -e "\x1B[0;01;32m>> ${test_file} <<\x1B[0m"
-  python3 ${test_file}
-done
